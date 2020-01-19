83bc3b9c72aea385bd853b561ebdab10f19f62a0
JoravDu <53528905+JoravDu@users.noreply.github.com>
2020-01-19 22:21:50 +0100

    update

---
+++ javascript/rms.js
@@ -0,0 +1,31 @@
+window.addEventListener('load', (event) => {
+    var Stamen_Toner = L.tileLayer('https://stamen-tiles-/%7Bs%7D.a.ssl.fastly.net/toner/%7Bz%7D/%7Bx%7D/%7By%7D%7Br%7D.%7Bext%7D', {
+        attribution: 'Map tiles by <a href="http://stamen.com/%22%3EStamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0%22%3ECC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright%22%3EOpenStreetMap</a> contributors',
+        subdomains: 'abcd',
+        minZoom: 0,
+        maxZoom: 20,
+        ext: 'png'
+    }),
+        OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright%22%3EOpenStreetMap</a> contributors'
+    });
+
+    var map = L.map('rms', {
+        center: [51.9720895, 4.0543201],
+        zoom: 9,
+        layers: [Stamen_Toner]
+    });
+
+    var baseMaps = {
+        "Openstreetmap": OpenStreetMap_Mapnik,
+        "Wit": Stamen_Toner
+    }
+
+    // Layer
+
+    var marker = L.marker([51.9720895, 4.0543201,]).bindPopup("Hier is de uitgang van de Rijn.").addTo(map);
+
+
+    L.control.layers(baseMaps).addTo(map);
+});
